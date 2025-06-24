7661c9bb90d0c465fba11acd9b593f719045eb2d
Kumar Mayank <kumarmayank2090@gmail.com>
2025-06-24 23:42:02 +0530

    Add files via upload

---
+++ maximumofthreenumber.java
@@ -0,0 +1,13 @@
+package corejavaprograme;
+
+public class maximumofthreenumber {
+	public static void main(String[] args) {
+		
+	
+	int a=40, b=20;
+	if(a>b) {
+		System.out.println("b");
+	}		
+}}
+
+
